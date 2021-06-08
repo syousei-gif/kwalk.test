75fb37c84660bfae5775f0ea8ba390e7bcfd1d0e
syousei-gif <77610482+syousei-gif@users.noreply.github.com>
2021-06-08 15:23:24 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+ <div class="container">
+ <!-- <a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/line.png" alt="line"></a>
+ <a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter.png" alt="twitter"></a>
+ <a href="" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/blog.png" alt="blog"></a> -->
+ <p class="text-center">Copyright © 株式会社K-walk All Rights Reserved.</p>
+<!-- <a href="" target="_blank">特定商取引法に基づく表記</a><br> -->
+<br>
+ </div>
+ </footer> 
+ <?php wp_footer(); ?>
+</body>
+</html>
